5b0c5d6377aef54d6b0abb62aed164b64260ecb4
nicolas <nicolas.gys@polytechnique.edu>
2020-02-26 15:18:23 +0100

    merge

--- game.py
+++ game.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import gym
 import blackjack as bj  # no joke
 
-
 env = bj.BlackjackEnv(nb_deck=1)
 gamma = 0.8
 epsilon = 0.01
@@ -16,10 +15,25 @@ def random_action():
     return env.action_space.sample()
 
 
-# deterministic action: winning score is 42% on average for sup=14
-def deterministic_action(player_hand, sup=18):
-    player_total = bj.sum_hand(player_hand)
-    if player_total < sup:
+# semi-random action: winning score is 37% on average
+def semi_random_action(player_score, count_cards, nb_cards_out):
+    card_sup = 21 - player_score
+    if card_sup >= 10:
+        return 1
+
+    proba = 4 * card_sup
+    for i in range(card_sup):
+        proba -= count_cards[i]
+    proba /= (52 - nb_cards_out)
+
+    if proba < 0.5:
+        return 0
+    return 1
+
+
+# strategic action: winning score is 42% on average for sup=14
+def strategic_action(player_hand, sup=18):
+    if player_hand < sup:
         return 1
     return 0
 
@@ -80,115 +94,114 @@ def basic_strategy(player_hand, dealer_value, soft):
 
 
 # sup is the maximum hand score in the deterministic action
-def main(algo='basic strategy', sup=18, Verbose=False):
+def main(algo, sup=18, Verbose=False):
     avg_win = 0
+    avg_tie = 0
     nb_games = 10000
 
     # for loop to run nb_games blackjack games
     for i_game in range(nb_games):
+        count_cards = np.zeros(10)  # we make no difference between 10/J/Q/K
         player_hand, dealer_first_card, usable_ace = env.reset()
+        for card in player_hand:
+            count_cards[card - 1] -= 1
+        count_cards[dealer_first_card - 1] -= 1
+
         reward = 0.
+
+    if Verbose:
+        print("Pass 0 - Player's score:", bj.sum_hand(player_hand))
+
+    # in the official rules, there can only be a maximum of 2 passes, but we will not implement this rule
+    # (not relevant)
+    # theoretically ,there cannot be more than 11 passes (4*aces, 4*two, 3*three)
+    for t in range(11):
+        # if we choose the 'random' strategy
+        if algo == 'random':
+            action = random_action()
+
+        # if we choose the 'semi-random' strategy
+        if algo == 'semi-random':
+            # for the first iteration, we need to sum the hand (it will be done in the loop later on)
+            if t == 0:
+                player_hand = bj.sum_hand(player_hand)
+            action = semi_random_action(player_hand, count_cards, 3 + t)
+
+        # if we choose the 'strategic' strategy
+        if algo == 'strategic':
+            # for the first iteration, we need to sum the hand (it will be done in the loop later on)
+            if t == 0:
+                player_hand = bj.sum_hand(player_hand)
+            action = strategic_action(player_hand, sup)
+
+        if algo == "basic strategy":
+            action = basic_strategy(player_hand, dealer_first_card, usable_ace)
+
+        observation, reward, done, info = env.step(action)
+        player_hand, dealer_first_card, usable_ace = observation
+
         if Verbose:
-            print("Pass 0 - Player's score:", bj.sum_hand(player_hand))
+            print("Pass {} - Player's score:".format(i_game + 1), player_hand)
+            if player_hand > 21:
+                print("Player has been busted.")
 
-        # in the official rules, there can only be a maximum of 2 passes, but we will not implement this rule
-        # (not relevant)
-        # theoretically ,there cannot be more than 11 passes (4*aces, 4*two, 3*three)
-        for t in range(11):
-            # if we choose the 'random' strategy
-            if algo == 'random':
-                action = random_action()
-
-                # if we choose the 'deterministic' strategy
-            if algo == 'deterministic':
-                # for the first iteration, we need to sum the hand (it will be done in the loop later on)
-                action = deterministic_action(player_hand, sup)
-
-            if algo == "basic strategy":
-                action = basic_strategy(player_hand, dealer_first_card, usable_ace)
-
-            '''
-            #if we choose the 'value iteration' strategy
-            if algo == 'value_iteration':
-                #building the probability matrix
-                M = np.zeros(18, 18)
-                for i in range(4, 22):
-                    for j in range(4, 22):
-                        M[i - 4, j - 4] = proba(i, j, player_hand, dealer_first_card)
-                v2 = np.zeros(18)
-
-                while delta < epsilon * (1 - gamma) / gamma:
-                    v1 = np.copy(v2)
-                    delta = 0
-
-                    #loop on every state possible
-                    for i in range(4, 22):
-                        if i == 21:
-                            v2[-1] = reward[-1]
-                        else:
-                            if_draw = M.dot(v1)[i - 4]
-                            if_not_draw = v1[i - 4]
-                            v2[i - 4] = reward[i - 4] + gamma * max(if_draw, if_not_draw)
-                        
-                        diff = abs(v2[s] - v1[s])
-                        if diff > delta:
-                            delta = diff    
-            '''
-
-            observation, reward, done, info = env.step(action)
-            player_hand, dealer_first_card, usable_ace = observation
-            #player_hand = bj.sum_hand(player_hand)
+        if done:
             if Verbose:
-                print("Pass {} - Player's score:".format(i_game + 1), player_hand)
-                if player_hand > 21:
-                    print("Player has been busted.")
+                dealer_hand = bj.score(env.dealer)
+                print("Dealer's score:", dealer_hand)
+                if dealer_hand > 21:
+                    print("Dealer has been busted.")
 
-            if done:
+            # if the player won the game
+            if reward == 1.:
+                avg_win += 1
                 if Verbose:
-                    dealer_hand = bj.score(env.dealer)
-                    print("Dealer's score:", dealer_hand)
-                    if dealer_hand > 21:
-                        print("Dealer has been busted.")
-
-                # if the player won the game
-                if reward == 1.:
-                    avg_win += 1
-                    if Verbose:
-                        print("GAME WON")
-                        print()
-                # if the player lost the game
-                else:
-                    if Verbose:
-                        print("GAME LOST")
-                        print()
-                break
+                    print("GAME WON")
+                    print()
+            # if the player lost the game
+            else:
+                if Verbose:
+                    print("GAME LOST")
+                    print()
+            break
 
     env.close()
 
     # percentage of winning games
-    return avg_win / nb_games
+    return 100 * avg_win / nb_games, 100 * avg_tie / nb_games
 
 
 # TEST
-# Random action
-print("Average winning score with random action:", main(algo='random'), "%")
 
-#basic strategy action
+# basic strategy action
 print("Average winning score with basic strategy action:", main(algo='basic strategy'), "%")
 
+# Random action
+random_algo = main(algo='random')
+print("Average winning score with random action:", random_algo[0], "%")
+print("Ties:", random_algo[1], "%  ||  Losses:", 100 - (random_algo[0] + random_algo[1]), "%")
+
+# Semi-random action
+semi_random_algo = main(algo='semi-random')
+print("Average winning score with semi-random action:", semi_random_algo[0], "%")
+print("Ties:", semi_random_algo[1], "%  ||  Losses:", 100 - (semi_random_algo[0] + semi_random_algo[1]), "%")
+
 # TEST
-# Deterministic action
+# Strategic action
 x = np.arange(22)
 y = []
 for i in x:
     # We try for every maximum hand score
-    y.append(main(algo='deterministic', sup=i))
+    y.append(main('strategic', sup=i))
 y = np.array(y)
-best_i = np.argmax(y)
-print("Best winning score of", y[best_i], "was obtained with maximum hand score of", best_i)
+print(y)
+print(y[:, 0])
+best_i = np.argmax(y, axis=0)[0]
+print("Best winning score of", y[best_i][0], "% was obtained with maximum hand score of", best_i)
+print("Ties:", y[best_i][1], "%  ||  Losses:", 100 - (y[best_i][0] + y[best_i][1]), "%")
 # And we plot the results
-plt.plot(x, y)
-plt.ylim((0, 1.0))
+plt.plot(x, y[:, 0])
+plt.ylim((0, 100))
 plt.xlabel("Maximum hand score before quitting", fontsize=16)
 plt.ylabel("Average winning score", fontsize=16)
 plt.show()
